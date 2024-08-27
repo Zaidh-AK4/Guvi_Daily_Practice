4b2161115db9eca59fff9b7fbf043f06d5b06b36
Zaidh-AK4 <zaidhabdulkather4@gmail.com>
2024-08-26 22:59:30 -0700

    Daily Task

---
+++ js_Practice.js
@@ -0,0 +1,176 @@
+  /* 
+  function functionName(name){
+    console.log("Hello, " + name + "!");
+  }
+
+  function double(number_to_doubled) {
+    console.log(number_to_doubled * 2);
+  }
+
+  function triple(num) {
+    return num * 3
+  }
+
+  double(12)
+  console.log(triple(12));
+
+  const multi = (a,b) => a*b;
+  console.log(multi(5,10)); 
+
+  const subtract = function(a,b){
+    return a - b;
+  }
+
+  console.log(subtract(5,10));
+
+  functionName('Zaidh') 
+  */
+
+  //const multiply = (x,y) => x*y;
+  //console.log(multiply(4,5)); 
+
+  // function func1(){
+  //   let x = 5;
+  //   return x
+  // }
+  // function func2(){
+  //   let y = 5 + xf;
+  //   console.log(y);
+  // }
+
+  // func1()
+  // xf = func1()
+  // func2()
+
+  // function func1(func){
+  //   func()
+  // }
+
+  // func1(function(){
+  //   console.log("CALLBACK");
+  // })
+
+  // const data = [12,8,50];
+  // function increaseArray(arr, n){
+  //   let result = [];
+
+  //   for (const element of arr){
+  //     result.push(element + arr);
+  //   }
+  //   return result;
+  // }
+
+  // const result = increaseArray(data,3);
+  // console.log(result);
+
+  // function top(){
+  //   console.log("top fun");
+
+  //   function button(){
+  //     console.log("bot fun");
+  //   }
+  // }
+
+  // function top(){
+  //   console.log("top fun");
+
+  //   function buttom(){
+  //     console.log("bot fun");
+  //   }
+  //   return buttom()
+  // }
+
+  // top()
+
+  // function func1(func){
+  //   console.log("This is the output for the first function");
+  //   func()
+  // }
+
+  // func1(function func2(){
+  //   console.log("2nd function Output")
+  // })
+
+  // function func1(func_upcoming){
+  //   console.log("this is the output for the func1");
+  //   func_upcoming();
+  //   func_upcoming();
+  //   func_upcoming();
+
+  // }
+
+  // function func2(){
+  //   console.log("Func2 output");
+  // }
+
+  // func1(func2)
+
+  // function func1(){
+  //   console.log("Output for func1");
+  //   return function(){
+  //     let x = 7,y = 8;
+  //     return x*y;
+  //   }
+  // }
+
+  // console.log(func1()())
+
+  // function recursion(count){
+  //   if (count === 0){
+  //     return console.log("The water is over!");
+  //   }
+  //   console.log("The water is fetching");
+  //   recursion(count-1);
+    
+  // }
+
+  // recursion(5);
+
+  // function f1(callback_fn){
+  //   callback_fn();
+  //   console.log("this is the f1 Output");
+    
+  // }
+
+  // function f2(){
+  //   console.log("F2 Output");
+  // }
+
+  // f1(f2)
+
+  // let colors = ['red', 'blue', 'green']
+  // console.log(colors);
+  // console.log(typeof(colors));
+
+  // let colour = Array('white', 'black', 'gray');
+  // console.log(colour);
+  // console.log(typeof colour);
+
+  // let fruits = Array('Apple', 'Mango', 'Banana');
+  // console.log(fruits);
+  // fruits.length = 2;
+  // console.log(fruits);
+  // console.log(fruits.length);
+
+  // let x = [[3,6,9],[2,4,6],[5,10,15]]
+  // console.log(x[1][2]);
+
+  // for (let i=0; i <= 5; i++){
+  //   console.log(`This is the Loop ${i}`);
+  // }
+  // console.log("Zaidh")
+
+  // let x = 0;
+  // while (x<=3){
+  //   console.log("While Loooping");
+  //   x++;
+  // }
+
+  // y = 0
+  // do {
+  //   console.log("Print this statement");
+  //   console.log("Print 2nd line")
+  //   y++
+  //  } while (y<4);
+
+
